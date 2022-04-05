a269117db0b56c6ca61bf7cf3646fe242a84f5cb
JJacobSV <17018@my.sanctamaria.school.nz>
2022-04-05 12:25:16 +1200

    Update fortnite_bot.py

---
+++ fortnite_bot.py
@@ -0,0 +1,13 @@
+# Fornite Costume Bot
+import random
+from random import randint
+
+#List of random names
+names = ["Bana", "Alvin", "Hannah", "Marquis", "Emily","Christy", "Carlos", "Theodore", "Simon", "Shaniqa"]
+
+num = randint(0,9) 
+
+name = (names[num])
+
+print(name)
+
