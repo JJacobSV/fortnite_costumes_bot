864615993b600b454082aaf0bce010af9f356c63
JJacobSV <17018@my.sanctamaria.school.nz>
2022-04-07 12:18:47 +1200

    Create welcome.py

---
+++ Components/welcome.py
@@ -0,0 +1,13 @@
+import random
+from random import randint
+
+#List of random names
+names = ["Bana", "Alvin", "Hannah", "Marquis", "Emily","Christy", "Carlos", "Theodore", "Simon", "Shaniqa"]
+
+num = randint(0,9) 
+
+name = (names[num])
+
+print("*** Welcome to Fortnite Battlepass skins ***")
+print("*** My name is",name, "***")
+print("*** I wil be here to help you order your FAVORITE Fortnite costumes ***")
