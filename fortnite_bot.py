6943252999247715a2eb83d2d856f2aff01b1885
JJacobSV <17018@my.sanctamaria.school.nz>
2022-04-10 12:45:29 +1200

    Added comments and integrated welcome message
    Commented each section needed

--- fortnite_bot.py
+++ fortnite_bot.py
@@ -3,11 +3,58 @@ import random
 from random import randint
 
 #List of random names
-names = ["Bana", "Alvin", "Hannah", "Marquis", "Emily","Christy", "Carlos", "Theodore", "Simon", "Shaniqa"]
+names = ["Banana", "Alvin", "Hannah", "Marquis", "Emily","Christy", "Carlos", "Theodore", "Simon", "Shaniqa"]
 
-num = randint(0,9) 
+# Welcome message with random name
+def welcome():
+    num = randint(0,9) 
+    name = (names[num])
+    print("*** Welcome to Fortnite Battlepass skins ***")
+    print("*** My name is",name, "***")
+    print("*** I wil be here to help you order your FAVORITE Fortnite costumes ***")
 
-name = (names[num])
 
-print(name)
+# Menu for pickup or delivery
 
+
+
+
+
+# Pick up information - name and phone number
+
+
+
+
+# Delivery function - name, address and phone
+
+
+
+
+# Choose total number of costumes - max 5
+
+
+
+# Costume Menu
+
+
+
+# Costume order - from menu - Print each costume ordered with cost
+
+
+
+
+# Print order out - including if order is del or pick up and names and price of each pizza - total cost including any delivery charge 
+
+
+# Ability to cancel or proceed with order
+
+
+
+# Option for new order or to exit
+
+
+# Main Function
+def main():
+    welcome()
+
+main()
