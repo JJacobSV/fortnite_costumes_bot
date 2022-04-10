ca5265581f8b1af4d37009ee3996e7af428b65e3
JJacobSV <17018@my.sanctamaria.school.nz>
2022-04-11 11:06:29 +1200

    Created print order v1
    Created version 1 of print order

---
+++ Components/order_printv1.py
@@ -0,0 +1,10 @@
+# List to store ordered costumes
+order_list =['Eon', 'Recon Expert', 'Aerial Assault Trooper', 'The Reaper']
+# List to store costume prices
+order_cost =[10.69, 19.99, 19.99, 10.69]
+
+
+count = 0
+for item in order_list:
+    print("Ordered:{} Cost ${:.2f}"  .format(item, order_cost[count]))
+    count = count+1
